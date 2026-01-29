24ed2035ab3486a5958ae7d7f4c99c1a9f86a752
Nixon <voxell.technologies@gmail.com>
2026-01-29 11:44:36 +0800

    Spatree query skeleton implementation

--- crates/rectree/src/spatree.rs
+++ crates/rectree/src/spatree.rs
@@ -1,3 +1,6 @@
+//! A spatial LBVH tree via Morton codes for hit collision detection.
+//! Reference: <https://developer.nvidia.com/blog/thinking-parallel-part-iii-tree-construction-gpu/>
+
 use core::ops::Deref;
 
 use alloc::vec::Vec;
@@ -11,6 +14,7 @@ pub struct Spatree {
     pub nodes: Vec<Node>,
 }
 
+// Builders.
 impl Spatree {
     pub fn new() -> Self {
         Self::default()
@@ -23,6 +27,7 @@ impl Spatree {
         RectId(index)
     }
 
+    /// Build node hierarchy and calculate their bounds.
     pub fn build(&mut self, point_from_rect: fn(&Rect) -> Point) {
         let internal_node_len = self.rects.len() - 1;
         if internal_node_len == 0 {
@@ -46,11 +51,12 @@ impl Spatree {
 
         morton_codes.sort_unstable();
 
-        // Top down hierarchy building for single threaded algorithm.
-        self.nodes = generate_hierarchy_iterative(&morton_codes);
+        // Build internal nodes.
+        self.nodes = generate_hierarchy(&morton_codes);
+        self.calculate_internal_bounds();
     }
 
-    pub fn calculate_bounds(&mut self) {
+    fn calculate_internal_bounds(&mut self) {
         if self.nodes.is_empty() {
             return;
         }
@@ -66,7 +72,7 @@ impl Spatree {
                 let child_rect = match child_id {
                     NodeId::Leaf(rect_id) => {
                         // Leaf bounds are already known from the input rects
-                        self.rects[*rect_id]
+                        self.rects[rect_id]
                     }
                     NodeId::Internal(idx) => {
                         // Because idx > i, this child's rect was
@@ -87,28 +93,70 @@ impl Spatree {
                 self.nodes[i].rect = final_rect;
             }
         }
+    }
+}
 
-        // Optionally update the global Spatree bound to the root's bound.
-        if let Some(root) = self.nodes.first() {
-            self.bound = root.rect;
+/// Queries.
+impl Spatree {
+    pub fn query<T>(
+        &self,
+        target: T,
+        hit_condition: fn(&Rect, &T) -> bool,
+    ) -> Vec<RectId> {
+        let mut hits = Vec::new();
+
+        if self.nodes.is_empty() {
+            // There's no tree, if there's just one rect, do a hit test for it.
+            if let Some(rect) = self.rects.first()
+                && hit_condition(rect, &target)
+            {
+                hits.push(RectId(0));
+            }
+        } else {
+            // Traveres the tree.
         }
+
+        hits
+    }
+
+    pub fn query_point(&self, point: Point) -> Vec<RectId> {
+        self.query(
+            point,
+            #[inline]
+            |rect, point| rect.contains(*point),
+        )
+    }
+
+    pub fn query_rect(&self, rect: Rect) -> Vec<RectId> {
+        self.query(
+            rect,
+            #[inline]
+            |rect, target_rect| rect.overlaps(*target_rect),
+        )
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Node {
     pub rect: Rect,
-    pub parent: NodeId,
+    pub parent: Option<usize>,
     pub children: [NodeId; 2],
 }
 
+impl Node {
+    pub const EMPTY: Self = Self {
+        rect: Rect::ZERO,
+        parent: None,
+        children: [NodeId::Invalid; 2],
+    };
+}
+
 #[derive(
-    Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,
 )]
 pub enum NodeId {
     Internal(usize),
-    Leaf(RectId),
-    #[default]
+    Leaf(usize),
     Invalid,
 }
 
@@ -125,11 +173,98 @@ impl Deref for RectId {
     }
 }
 
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,
+)]
+pub struct MortonCode {
+    pub code: u32,
+    pub index: usize,
+}
+
+/// Top down hierarchy building for single threaded algorithm.
+pub fn generate_hierarchy(codes: &[MortonCode]) -> Vec<Node> {
+    let len = codes.len();
+    if len <= 1 {
+        return Vec::new();
+    }
+
+    // A binary tree with N leaves has exactly N - 1 internal nodes.
+    let mut internal_nodes = vec![Node::EMPTY; len - 1];
+
+    /// Represents a range to be split and its connection to the tree.
+    struct BuildStack {
+        first: usize,
+        last: usize,
+        parent_idx: Option<usize>,
+        /// `0` for left, `1` for right.
+        child_slot: usize,
+    }
+
+    let mut stack = Vec::with_capacity(len);
+    let mut node_idx = 0;
+
+    // First build stakc will have the full range.
+    stack.push(BuildStack {
+        first: 0,
+        last: internal_nodes.len(),
+        parent_idx: None,
+        child_slot: 0,
+    });
+
+    while let Some(task) = stack.pop() {
+        let BuildStack {
+            first,
+            last,
+            parent_idx,
+            child_slot,
+        } = task;
+
+        let curr_node_id = if first == last {
+            // Single element range represents a leaf node.
+            NodeId::Leaf(codes[first].index)
+        } else {
+            // Internal node case.
+            let node_id = NodeId::Internal(node_idx);
+            let split = find_split(codes, first, last);
+
+            // Push right sub-range then left sub-range (LIFO).
+            stack.push(BuildStack {
+                first,
+                last: split,
+                parent_idx: Some(node_idx),
+                child_slot: 0,
+            });
+            stack.push(BuildStack {
+                first: split + 1,
+                last,
+                parent_idx: Some(node_idx),
+                child_slot: 1,
+            });
+
+            node_idx += 1;
+            node_id
+        };
+
+        // Link the current node to its parent if it's not the root.
+        if let Some(parent_idx) = parent_idx {
+            internal_nodes[parent_idx].children[child_slot] =
+                curr_node_id;
+
+            // If the current node is internal, set its parent index.
+            if let NodeId::Internal(curr_idx) = curr_node_id {
+                internal_nodes[curr_idx].parent = Some(parent_idx);
+            }
+        }
+    }
+
+    internal_nodes
+}
+
 /// `x` & `y` must be within the `0..=1` range.
 pub fn morton_2d_f64(x: f64, y: f64) -> u32 {
     const MAX: f64 = 65535.0;
-    let x = (x * MAX) as u16;
-    let y = (y * MAX) as u16;
+    let x = (x.clamp(0.0, 1.0) * MAX) as u16;
+    let y = (y.clamp(0.0, 1.0) * MAX) as u16;
 
     morton_2d(x, y)
 }
@@ -153,20 +288,21 @@ pub const fn find_split(
 ) -> usize {
     let first_code = morton_codes[first].code;
     let last_code = morton_codes[last].code;
-    // Handle duplicated morton code separately.
+    // Split the range in the middle for identical Morton codes.
     if first_code == last_code {
-        return (first + last) / 2;
+        return (first + last) >> 1;
     };
 
-    let common_prefix = (first_code ^ last_code).leading_zeros();
+    let common_prefix = calc_common_prefix(first_code, last_code);
 
     // Use binary search to find where the next bit differs.
     // Specifically, we are looking for the highest object that
     // shares more than `common_prefix` bits with the first one.
 
-    let mut split = first; // initial guess
+    // Initial guess.
+    let mut split = first;
     let mut step = last - first;
-    loop {
+    while step > 1 {
         // Exponential decrease.
         step = (step + 1) >> 1;
         // Proposed new position.
@@ -175,127 +311,20 @@ pub const fn find_split(
         if new_split < last {
             let split_code = morton_codes[new_split].code;
             let split_prefix =
-                (first_code ^ split_code).leading_zeros();
+                calc_common_prefix(first_code, split_code);
 
             if split_prefix > common_prefix {
                 // Accept proposal.
                 split = new_split
             };
         }
-
-        if step <= 1 {
-            break;
-        }
     }
 
     split
 }
 
-/// Delta operator measures the common prefix of two morton codes if
-/// `j` is not in the range of the morton code array, delta operator
-/// returns `None`.
-pub const fn delta(
-    i: usize,
-    j: usize,
-    morton_codes: &[MortonCode],
-) -> u32 {
-    (morton_codes[i].code ^ morton_codes[j].code).leading_zeros()
-}
-
-#[derive(
-    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,
-)]
-pub struct MortonCode {
-    pub code: u32,
-    pub index: usize,
-}
-
-pub fn generate_hierarchy_iterative(
-    codes: &[MortonCode],
-) -> Vec<Node> {
-    let len = codes.len();
-    if len <= 1 {
-        return Vec::new();
-    }
-
-    // A binary tree with N leaves has exactly N - 1 internal nodes.
-    let mut internal_nodes = vec![
-        Node {
-            rect: Rect::default(),
-            parent: NodeId::Invalid,
-            children: [NodeId::Invalid; 2],
-        };
-        len - 1
-    ];
-
-    let mut stack = Vec::with_capacity(len);
-    let mut next_internal_idx = 0;
-
-    /// Represents a range to be split and its connection to the tree.
-    struct BuildTask {
-        first: usize,
-        last: usize,
-        parent_idx: Option<usize>,
-        /// `0` for left, `1` for right.
-        child_slot: usize,
-    }
-
-    // Push the root task: the full range of sorted Morton codes.
-    stack.push(BuildTask {
-        first: 0,
-        last: len - 1,
-        parent_idx: None,
-        child_slot: 0,
-    });
-
-    while let Some(task) = stack.pop() {
-        let BuildTask {
-            first,
-            last,
-            parent_idx,
-            child_slot,
-        } = task;
-
-        let current_node_id = if first == last {
-            // Is leaf node: use the index stored in `MortonCode` .
-            NodeId::Leaf(RectId(codes[first].index))
-        } else {
-            // Internal node case.
-            let node_idx = next_internal_idx;
-            next_internal_idx += 1;
-
-            let split = find_split(codes, first, last);
-
-            // Push right sub-range then left sub-range (LIFO)
-            stack.push(BuildTask {
-                first: split + 1,
-                last,
-                parent_idx: Some(node_idx),
-                child_slot: 1,
-            });
-
-            stack.push(BuildTask {
-                first,
-                last: split,
-                parent_idx: Some(node_idx),
-                child_slot: 0,
-            });
-
-            NodeId::Internal(node_idx)
-        };
-
-        // Link the current node to its parent if it's not the root
-        if let Some(parent_idx) = parent_idx {
-            internal_nodes[parent_idx].children[child_slot] =
-                current_node_id;
-
-            // If the current node is internal, set its parent pointer
-            if let NodeId::Internal(curr_idx) = current_node_id {
-                internal_nodes[curr_idx].parent =
-                    NodeId::Internal(parent_idx);
-            }
-        }
-    }
-
-    internal_nodes
+/// Measures the common prefix of two morton codes.
+#[inline]
+pub const fn calc_common_prefix(code_a: u32, code_b: u32) -> u32 {
+    (code_a ^ code_b).leading_zeros()
 }
